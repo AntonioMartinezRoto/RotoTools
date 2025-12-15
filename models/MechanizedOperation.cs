fe7392a98a877ba42671075a5a96ef73df51f0e1
Antonio Martínez <antonio.martinez@roto-frank.com>
2025-12-15 11:47:41 +0100

    Inicialización Level2 de las operaciones por tipo.

--- models/MechanizedOperation.cs
+++ models/MechanizedOperation.cs
@@ -40,6 +40,69 @@ namespace RotoEntities
             Level1 = "ROTO";
             RGB = 255;
             Disable = false;
+            InitializeLevel2(operationName);
+        }
+
+        private void InitializeLevel2(string operationName)
+        {
+            string op = operationName.ToUpper();
+
+            if (op.Contains("BOMBILLO"))
+            {
+                Level2 = "BOMBILLOS";
+            }
+            else if (op.Contains("PERNIO") || op.Contains("BISAGRA") || op.Contains("PB10") || op.Contains("150R") || op.Contains("SOPORTE_COMPAS"))
+            {
+                Level2 = "BISAGRAS";
+            }
+            else if (op.Contains("BOCALLAVE"))
+            {
+                Level2 = "BOCALLAVES";
+            }
+            else if (op.Contains("TRIPLE") || op.Contains("CREMONA") || op.Contains("X3") || op.Contains("TALADRO") || op.Contains("UÑERO") || op.Contains("PALANCA"))
+            {
+                Level2 = "CREMONAS";
+            }
+            else if (op.Contains("MANILLA"))
+            {
+                Level2 = "MANILLAS";
+            }
+            else if (op.Contains("PLACA"))
+            {
+                Level2 = "PLACAS";
+            }
+            else if (op.Contains("CERRADURA") || op.Contains("MOTOR") || op.Contains("ENEO") || op.Contains("PLETINA"))
+            {
+                Level2 = "CERRADURAS";
+            }
+            else if (op.Contains("CERR_") || op.Contains("MRC_"))
+            {
+                Level2 = "CERRADEROS";
+            }
+            else if (op.Contains("TANDEM"))
+            {
+                Level2 = "RUEDAS";
+            }
+            else if (op.Contains("ALV_"))
+            {
+                Level2 = "ALVERSA";
+            }
+            else if (op.Contains("ELEV_"))
+            {
+                Level2 = "PATIO LIFT";
+            }
+            else if (op.Contains("PLEG_"))
+            {
+                Level2 = "PLEGABLES";
+            }
+            else if (op.Contains("INOWA"))
+            {
+                Level2 = "INOWA";
+            }
+            else
+            {
+                Level2 = "OTRAS";
+            }
         }
     }
 }
